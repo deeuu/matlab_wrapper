a14ff3b4bfb47b44d18b76bc2a9fe92381ff4a16
deeuu <deeuu@users.noreply.github.com>
2017-07-14 17:28:08 +0100

    Pep8

--- tests/test_matlab.py
+++ tests/test_matlab.py
@@ -34,11 +34,9 @@ def matlab():
     return matlab
 
 
-
 def test_eval_ok(matlab):
     command = "a = ones(10)"
     matlab.eval(command)
-
 
 
 def test_eval_error(matlab):
@@ -65,7 +63,7 @@ def test_longscript(matlab):
 def test_get_numeric(matlab):
     for dtype in NUMERIC_DTYPES:
 
-        a = np.eye(4,5, dtype=dtype)
+        a = np.eye(4, 5, dtype=dtype)
         matlab.eval("b = eye(4,5, '{}')".format(dtype))
         b = matlab.get('b')
 
@@ -74,7 +72,7 @@ def test_get_numeric(matlab):
 
 
 def test_get_logical(matlab):
-    a = np.eye(4,5, dtype='bool')
+    a = np.eye(4, 5, dtype='bool')
     matlab.eval("b = eye(4,5) > 0")
     b = matlab.get('b')
 
@@ -84,7 +82,7 @@ def test_get_logical(matlab):
 
 def test_get_comples(matlab):
     for dtype in ('single', 'double'):
-        a = np.eye(4,5, dtype=dtype) + np.eye(4,5, dtype=dtype)*1j
+        a = np.eye(4, 5, dtype=dtype) + np.eye(4, 5, dtype=dtype)*1j
         matlab.eval("b = eye(4,5, '{0}') + eye(4,5, '{0}')*j".format(dtype))
         b = matlab.get('b')
 
@@ -106,10 +104,9 @@ def test_get_float(matlab):
     assert_equal(a, 1.)
 
 
-
 def test_put_numeric(matlab):
     for dtype in NUMERIC_DTYPES:
-        a = np.random.randn(2,3) * 10
+        a = np.random.randn(2, 3) * 10
         a = a.astype(dtype)
 
         matlab.put('a', a)
@@ -119,13 +116,13 @@ def test_put_numeric(matlab):
 
         numbers = output.split()[-6:]
         numbers = np.array(numbers, dtype=dtype)
-        numbers.shape = (2,3)
+        numbers.shape = (2, 3)
 
         assert_almost_equal(a, numbers, decimal=4)
 
 
 def test_put_logical(matlab):
-    a = np.random.randn(2,3)>0
+    a = np.random.randn(2, 3) > 0
 
     matlab.put('a', a)
     matlab.eval('a')
@@ -134,16 +131,14 @@ def test_put_logical(matlab):
 
     numbers = output.split()[-6:]
     numbers = np.array(numbers, dtype=int).astype('bool')
-    numbers.shape = (2,3)
+    numbers.shape = (2, 3)
 
     assert_equal(a, numbers)
 
 
-
-
 def test_put_complex128(matlab):
     for dtype in ('complex128', 'complex64'):
-        nums = np.linspace(0.1, 10, 6).reshape(2,3)
+        nums = np.linspace(0.1, 10, 6).reshape(2, 3)
 
         a = nums + nums*1j
         a = a.astype(dtype)
@@ -153,9 +148,12 @@ def test_put_complex128(matlab):
 
         output = matlab.output_buffer.split('\n')
 
-        assert_equal(output[3], '   0.1000 + 0.1000i   2.0800 + 2.0800i   4.0600 + 4.0600i')
-        assert_equal(output[4], '   6.0400 + 6.0400i   8.0200 + 8.0200i  10.0000 +10.0000i')
-
+        assert_equal(
+            output[3],
+            '   0.1000 + 0.1000i   2.0800 + 2.0800i   4.0600 + 4.0600i')
+        assert_equal(
+            output[4],
+            '   6.0400 + 6.0400i   8.0200 + 8.0200i  10.0000 +10.0000i')
 
 
 def test_put_string(matlab):
@@ -169,8 +167,6 @@ def test_put_string(matlab):
     assert_equal(s, "asdf")
 
 
-
-
 def test_put_unicode(matlab):
     matlab.put('s', u"Łódź")
     matlab.eval('s')
@@ -180,7 +176,6 @@ def test_put_unicode(matlab):
     s = output[-1]
 
     assert_equal(s, u"Łódź")
-
 
 
 def test_put_unicode_len(matlab):
@@ -195,8 +190,6 @@ def test_put_unicode_len(matlab):
     assert_equal(length, len(s))
 
 
-
-
 def test_put_float(matlab):
     matlab.put('a', 3.2)
     matlab.eval('a')
@@ -208,11 +201,9 @@ def test_put_float(matlab):
     assert_equal(a, 3.2)
 
 
-
-
 def test_put_get_numeric(matlab):
     for dtype in NUMERIC_DTYPES:
-        a = np.random.randn(3,2,4) * 10
+        a = np.random.randn(3, 2, 4) * 10
         a = a.astype(dtype)
 
         matlab.put('a', a)
@@ -222,9 +213,8 @@ def test_put_get_numeric(matlab):
         assert_equal(a, aa)
 
 
-
 def test_put_get_logical(matlab):
-    a = np.random.randn(3,2,4)>0
+    a = np.random.randn(3, 2, 4) > 0
 
     matlab.put('a', a)
     aa = matlab.get('a')
@@ -235,15 +225,14 @@ def test_put_get_logical(matlab):
 
 def test_put_get_complex(matlab):
     for datatype in ("complex64", "complex128"):
-        a = np.random.randn(2,4,3) + np.random.randn(2,4,3)*1j
+        a = np.random.randn(2, 4, 3) + np.random.randn(2, 4, 3)*1j
         a = a.astype(datatype)
 
-        matlab.put('a',a)
+        matlab.put('a', a)
         aa = matlab.get('a')
 
         assert_equal(a.dtype, aa.dtype)
         assert_equal(a, aa)
-
 
 
 def test_put_get_string(matlab):
@@ -253,7 +242,6 @@ def test_put_get_string(matlab):
     ss = matlab.get('s')
 
     assert_equal(s, ss)
-
 
 
 def test_workspace_func(matlab):
@@ -266,13 +254,12 @@ def test_workspace_func(matlab):
     assert_equal(y, ymatlab)
 
 
-
 def test_workspace_nout(matlab):
-    a = np.array([2,1,3])
+    a = np.array([2, 1, 3])
 
-    y,i = matlab.workspace.sort(a, nout=2)
+    y, i = matlab.workspace.sort(a, nout=2)
 
-    assert_equal(y, [1,2,3])
+    assert_equal(y, [1, 2, 3])
     assert_equal(i, a)
 
 
@@ -302,7 +289,7 @@ def test_get_cell(matlab):
     target = np.array(
         [
             [1., 2., 3.],
-            ['text', np.eye(2,3), np.array([11.,22.,33.], dtype='O')]
+            ['text', np.eye(2, 3), np.array([11., 22., 33.], dtype='O')]
         ],
         dtype='O'
     )
@@ -310,8 +297,8 @@ def test_get_cell(matlab):
     assert_equal(actual.shape, target.shape)
     assert_equal(actual.dtype, target.dtype)
 
-    for a,t in zip(actual.flatten(),target.flatten()):
-        assert_equal(a,t)
+    for a, t in zip(actual.flatten(), target.flatten()):
+        assert_equal(a, t)
 
 
 def test_get_uninitialized_cell(matlab):
@@ -327,23 +314,21 @@ def test_get_uninitialized_cell(matlab):
     assert_equal(actual.shape, target.shape)
     assert_equal(actual.dtype, target.dtype)
 
-    for a,t in zip(actual.flatten(),target.flatten()):
-        assert_equal(a,t)
-
+    for a, t in zip(actual.flatten(), target.flatten()):
+        assert_equal(a, t)
 
 
 def test_put_cell(matlab):
 
-    sub = np.array([3, 4.,'b'], dtype='O')
+    sub = np.array([3, 4., 'b'], dtype='O')
     c = np.array([
-        [1 , 'a'],
+        [1, 'a'],
         [2., sub]
     ], dtype='O')
 
-
     matlab.put('c', c)
 
-    ### check the main array
+    # check the main array
     matlab.eval('c')
 
     output = matlab.output_buffer.split('\n')
@@ -351,8 +336,7 @@ def test_put_cell(matlab):
     assert_equal(output[3], "    [1]    'a'       ")
     assert_equal(output[4], "    [2]    {1x3 cell}")
 
-
-    ### check the sub-array
+    # check the sub-array
     matlab.eval('c{2,2}')
 
     output = matlab.output_buffer.split('\n')
@@ -360,15 +344,12 @@ def test_put_cell(matlab):
     assert_equal(output[3], "    [3]    [4]    'b'")
 
 
-
-
-
 def test_put_get_cell(matlab):
 
     a = np.array(
         [
             [1., 2., 3.],
-            ['text', np.eye(2,3), np.array([11.,22.,33.], dtype='O')]
+            ['text', np.eye(2, 3), np.array([11., 22., 33.], dtype='O')]
         ],
         dtype='O'
     )
@@ -376,11 +357,8 @@ def test_put_get_cell(matlab):
     matlab.workspace.a = a
     aa = matlab.workspace.a
 
-    for el,elel in zip(a.flatten(),aa.flatten()):
-        assert_equal(el,elel)
-
-
-
+    for el, elel in zip(a.flatten(), aa.flatten()):
+        assert_equal(el, elel)
 
 
 def test_get_struct(matlab):
@@ -403,7 +381,6 @@ def test_get_struct(matlab):
     assert_equal(s, desired)
 
 
-
 def test_get_uninitialized_struct(matlab):
 
     matlab.eval("""
@@ -415,14 +392,12 @@ def test_get_uninitialized_struct(matlab):
 
     s = matlab.get('s')
 
-
     desired = np.array([
         [(1, None), (None, None)],
         [(None, None), (2, 'a')]
     ], dtype=[('x', 'O'), ('y', 'O')])
 
     assert_equal(s, desired)
-
 
 
 def test_get_empty_struct(matlab):
@@ -436,7 +411,6 @@ def test_get_empty_struct(matlab):
     assert_equal(s, desired)
 
 
-
 def test_put_struct(matlab):
 
     a = np.rec.fromrecords([
@@ -446,7 +420,7 @@ def test_put_struct(matlab):
 
     matlab.put('a', a)
 
-    ### 1st element
+    # 1st element
     matlab.eval('a(1)')
 
     output = matlab.output_buffer.split('\n')
@@ -455,8 +429,7 @@ def test_put_struct(matlab):
     assert_equal(output[4], "    f1: 'a'")
     assert_equal(output[5], "    f2: 1")
 
-
-    ### 2nd element
+    # 2nd element
     matlab.eval('a(2)')
 
     output = matlab.output_buffer.split('\n')
@@ -464,8 +437,6 @@ def test_put_struct(matlab):
     assert_equal(output[3], "    f0: 2")
     assert_equal(output[4], "    f1: 'bb'")
     assert_equal(output[5], "    f2: 2")
-
-
 
 
 def test_put_get_struct(matlab):
@@ -482,7 +453,6 @@ def test_put_get_struct(matlab):
     assert_equal(a, aa)
 
 
-
 def test_put_strings(matlab):
     s = ['asdf', 'a', 'BBB']
 
@@ -492,8 +462,6 @@ def test_put_strings(matlab):
     output = matlab.output_buffer.split('\n')
 
     assert_equal(output[3], "    'asdf'    'a'    'BBB'")
-
-
 
 
 def test_put_get_dataframe(matlab):
@@ -508,10 +476,13 @@ def test_put_get_dataframe(matlab):
     desired = np.array([
         (0, 1.0, 2, 'asdf'),
         (1, 1.1, 4, 'marek')
-    ], dtype=[('index', '<i8'), ('a', '<f8'), ('b', '<i8'), ('c', six.text_type, 5)])
+    ], dtype=[('index', '<i8'),
+              ('a', '<f8'),
+              ('b', '<i8'),
+              ('c', six.text_type, 5)]
+    )
 
     assert_equal(a, desired)
-
 
 
 def test_put_get_series(matlab):
